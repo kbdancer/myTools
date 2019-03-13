380f07006b374ec63df5619d2355b23e2f9ca7f7
kbdancer <root@92ez.com>
2019-03-13 17:50:52 +0800

    Python3 改写

--- cutIPrange/cutRange.py
+++ cutIPrange/cutRange.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
-# code by kbdancer@92ez.com
+# code by kbdancer
+# Blog 92ez.com
 
 import sys
 
@@ -15,14 +16,14 @@ if __name__ == '__main__':
         for line in file_object:
             range_list.append(line)
             temp_count += 1
-            if temp_count == int(sys.argv[1]):
+            if temp_count == int(50):
                 temp_count = 0
                 page_index += 1
                 write_path = sys.path[0] + "/rangeFiles/range" + str(page_index) + ".txt"
 
                 with open(write_path, 'wb') as f:
                     for host in range_list:
-                        f.write(host)
+                        f.write(host.encode('utf8'))
                     f.flush()
                     f.close()
 
@@ -32,9 +33,9 @@ if __name__ == '__main__':
             write_path = sys.path[0] + "/rangeFiles/range" + str(page_index) + ".txt"
             with open(write_path, 'wb') as f:
                 for host in range_list:
-                    f.write(host)
+                    f.write(host.encode('utf8'))
                 f.flush()
                 f.close()
         file_object.close()
-    except Exception, e:
-        print e
+    except Exception as e:
+        print(e)
